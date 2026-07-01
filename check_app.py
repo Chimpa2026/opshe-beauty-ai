61704c54e5bc4d80fb81e5319aeb8d69eded9891
Chimpa2026 <borneobotani2018@gmail.com>
2026-07-01 18:15:21 +0700

    Clean: remove temporary fix scripts

--- check_app.py
+++
@@ -1,4 +0,0 @@
-with open('frontend/static/js/app.js', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, l in enumerate(lines[:80], 1):
-    print(f'{i}: {l}', end='')
--- logo_b64.py
+++
@@ -1,6 +0,0 @@
-import base64
-with open('frontend/static/assets/logo.png', 'rb') as f:
-    data = base64.b64encode(f.read()).decode()
-with open('frontend/static/assets/logo_b64.txt', 'w') as f:
-    f.write(data)
-print('Done! Panjang:', len(data))
